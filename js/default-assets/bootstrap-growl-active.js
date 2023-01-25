502b723c8e60ba6649219d4d28c865509727eb8a
geekmakersun <geek@13aq.com>
2023-01-25 16:46:19 +0800

    优化

--- js/default-assets/bootstrap-growl-active.js
+++ js/default-assets/bootstrap-growl-active.js
@@ -1,20 +1,27 @@
 (function ($) {
     "use strict";
-    
+
     // :: Notify Message 激活代码
     function notify(message, type) {
         $.notify({
-            message: message
+            icon: 'ti-bell',
+            title: '萨曼莎莎拉',
+            message: message,
+            url: 'https://github.com/mouse0270/bootstrap-notify',
+            target: '_blank',
         }, {
             type: type,
-            allow_dismiss: false,
             label: 'Cancel',
             className: 'btn-xs btn-inverse',
             placement: {
                 from: 'top',
                 align: 'right'
             },
-            delay: 6000,
+            delay: 4000,
+            z_index: 99999,
+            spacing: 5,
+            showProgressbar: false, //显示进度条
+            allow_dismiss: true, //显示删除按钮
             animate: {
                 enter: 'animated fadeInRight',
                 exit: 'animated fadeOutRight'
@@ -25,6 +32,7 @@
             }
         });
     }
-    notify('您好，欢迎回来！ - Admetro 仪表板', 'inverse');
-    
+
+    notify('您好，欢迎回来！ - Admetro 仪表板', 'info');
+
 })(jQuery);
